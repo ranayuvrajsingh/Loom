de20ba9ae783cc77a51e128cc4e6474c48a1b441
ranayuvrajsingh <ranayuvrajsingh0000@gmail.com>
2024-04-14 14:43:50 +0530

    initial

--- constants/index.ts
+++ constants/index.ts
@@ -16,7 +16,7 @@ export const sidebarLinks = [
   },
   {
     label: "Recordings",
-    imgUrl: "icons/Recordings.svg",
+    imgUrl: "icons/Video.svg",
     route: "/recordings",
   },
   {
@@ -25,6 +25,35 @@ export const sidebarLinks = [
     route: "/personal-room",
   },
 ];
+
+// export const sidebarLinks = [
+//   {
+//     imgURL: "/icons/Home.svg",
+//     route: "/",
+//     label: "Home",
+//   },
+
+//   {
+//     imgURL: "/icons/upcoming.svg",
+//     route: "/upcoming",
+//     label: "Upcoming",
+//   },
+//   {
+//     imgURL: "/icons/previous.svg",
+//     route: "/previous",
+//     label: "Previous",
+//   },
+//   {
+//     imgURL: "/icons/Video.svg",
+//     route: "/recordings",
+//     label: "Recordings",
+//   },
+//   {
+//     imgURL: "/icons/add-personal.svg",
+//     route: "/personal-room",
+//     label: "Personal Room",
+//   },
+// ];
 
 export const avatarImages = [
   "/images/avatar-1.jpeg",
